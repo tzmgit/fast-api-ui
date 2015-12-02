6930666f0cc87dad0628c3930a1c7a2fd85cb77d
Zhuoming Tang <ztang@dataxu.com>
2015-12-02 14:45:06 -0500

    init app

---
+++ tests/factories.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Factories to help in tests."""
+from factory import PostGenerationMethodCall, Sequence
+from factory.alchemy import SQLAlchemyModelFactory
+
+from fast_api_ui.database import db
+from fast_api_ui.user.models import User
+
+
+class BaseFactory(SQLAlchemyModelFactory):
+    """Base factory."""
+
+    class Meta:
+        """Factory configuration."""
+
+        abstract = True
+        sqlalchemy_session = db.session
+
+
+class UserFactory(BaseFactory):
+    """User factory."""
+
+    username = Sequence(lambda n: 'user{0}'.format(n))
+    email = Sequence(lambda n: 'user{0}@example.com'.format(n))
+    password = PostGenerationMethodCall('set_password', 'example')
+    active = True
+
+    class Meta:
+        """Factory configuration."""
+
+        model = User
